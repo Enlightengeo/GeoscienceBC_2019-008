3f5ab1fc849ba11c422bd2e93e3443368ef295d8
Scott McKean <scott.mckean@ucalgary.ca>
2020-08-22 22:24:21 -0600

    repull final data, start modelling and tuning

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -13,3 +13,28 @@ most_freq_var <- function(df, var){
     dplyr::slice(which.max(n)) %>%
     dplyr::pull(var)
 }
+
+#' Calculate the bias and variance for a resample object
+#' 
+#' @param resample resample object from mlr::resample, predict must be 'both'
+#' @return list of train/test bias and variance + resample object for rds stash
+#' @export
+get_resample_class_res = function(resample){
+  train = resample$pred$data %>%
+    filter(set=='train') %>%
+    mutate(bias = ifelse(truth==response,0,1),
+           variance = ifelse(response==1, prob.0,prob.1))
+  
+  test = resample$pred$data %>%
+    filter(set=='test') %>%
+    mutate(bias = ifelse(truth==response,0,1),
+           variance = ifelse(response==1, prob.0,prob.1))
+  
+  return(
+    list(train_mean_bias=mean(train$bias), 
+         train_mean_variance=mean(train$variance),
+         test_mean_bias=mean(test$bias), 
+         test_mean_variance=mean(test$variance),
+         resample_obj = resample)
+  )
+}
